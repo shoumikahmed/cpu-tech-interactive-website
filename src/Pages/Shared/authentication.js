7d7701ee7f92809111c0e54d9510b12a44bf59b2
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-21 22:25:36 +0600

    jsnfi

---
+++ src/Pages/Shared/authentication.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const authentication = () => {
+    return (
+        <div>
+            <h1>dfjghfdj</h1>
+        </div>
+    );
+};
+
+export default authentication;
