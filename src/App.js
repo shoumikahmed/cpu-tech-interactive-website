2d3f4380d375544b06fe2db9b2d62fc792549cc4
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-22 14:10:27 +0600

    login page complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navber from './Pages/Shared/Navber';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Footer from './Pages/Shared/Footer';
+import NotFound from './Pages/Shared/NotFound';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/Login/SignUp';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/Shared/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1 className='text-center my-20 text-red-500 text-7xl'>404.NOT FOUND!!!</h1>
+        </div>
+    );
+};
+
+export default NotFound;
