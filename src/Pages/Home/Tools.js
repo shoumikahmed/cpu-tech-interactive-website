92e01aca78fa51450ed4e9c2d74a734ca8312272
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-22 15:38:10 +0600

    fetched tools.json

---
+++ src/Pages/Home/Tools.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import Tool from './Tool';
+
+const Tools = () => {
+    const [tools, setTools] = useState([])
+
+    useEffect(() => {
+        fetch('tools.json')
+            .then(res => res.json())
+            .then(data => setTools(data))
+    }, [])
+
+    return (
+        <div className='my-28'>
+            <div className='text-center '>
+                <h1 className='text-primary text-xl font-bold uppercase'>Our Services</h1>
+                <h2 className='text-4xl'>Services We Provide</h2>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                {
+                    tools.map(service => <Tool
+                        key={service._id}
+                        service={service}
+                    ></Tool>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Tools;
