d35cf39f00c722b519dd75d620bafeb97a0d727e
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-25 23:36:20 +0600

    order now field has been added

--- src/Pages/Home/Purchase.js
+++ src/Pages/Home/Purchase.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Purchase = () => {
+    const [user] = useAuthState(auth)
     const { id } = useParams()
     const [product, setProduct] = useState({})
 
@@ -24,21 +27,107 @@ const Purchase = () => {
 
 
     return (
-        <div className="card lg:max-w-lg bg-base-100 shadow-xl mt-20 hover:shadow-2xl mx-auto my-20">
-            <figure className='px-10 pt-10'>
-                <img src={product?.img} alt="" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">{product?.name}</h2>
-                <p>Min quantity: {product?.minimumquantity}</p>
-                <p>Available quantity: {product?.availablequantity}</p>
-                <p>Price: {product?.price}</p>
-                <p>{product?.description}</p>
-                <div class="card-actions justify-end">
-                    <button onClick={() => navigateToToolDetail(product?._id)} class="btn btn-primary">Place Order</button>
+        <section>
+            <div className="card w-96 lg:max-w-lg bg-base-100 shadow-xl mt-20 hover:shadow-2xl mx-auto my-20">
+                <figure className='px-10 pt-10'>
+                    <img src={product?.img} alt="" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">{product?.name}</h2>
+                    <p>Min quantity: {product?.minimumquantity}</p>
+                    <p>Available quantity: {product?.availablequantity}</p>
+                    <p>Price: {product?.price}</p>
+                    <p>{product?.description}</p>
                 </div>
             </div>
-        </div>
+
+
+            <div className='flex justify-center items-center mt-4 my-16'>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="text-center text-2xl font-bold">Order Now</h2>
+
+                        <form>
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text"></span>
+                                </label>
+                                <input
+                                    disabled
+                                    value={user?.displayName || ''}
+                                    name="name"
+                                    id="name"
+                                    type="text"
+                                    placeholder="Your Name"
+                                    className="input input-bordered w-full max-w-xs"
+                                />
+
+                            </div>
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text"></span>
+                                </label>
+                                <input
+                                    disabled
+                                    value={user?.email || ''}
+                                    name="email"
+                                    id="email"
+                                    type="email"
+                                    placeholder="Your Name"
+                                    className="input input-bordered w-full max-w-xs"
+                                />
+
+                            </div>
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text"></span>
+                                </label>
+                                <input
+                                    name="country"
+                                    id="country"
+                                    type="text"
+                                    placeholder="Country"
+                                    className="input input-bordered w-full max-w-xs"
+                                />
+
+                            </div>
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text"></span>
+                                </label>
+                                <input
+                                    name="address"
+                                    id="address"
+                                    type="text"
+                                    placeholder="Address"
+                                    className="input input-bordered w-full max-w-xs"
+                                />
+
+                            </div>
+
+
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text"></span>
+                                </label>
+                                <input
+                                    name="phone"
+                                    id="phone"
+                                    type="number"
+                                    placeholder="Phone Number"
+                                    className="input input-bordered w-full max-w-xs"
+                                />
+
+                            </div>
+
+                            <input className='btn w-full max-w-xs mt-5' type="submit" value="Order Now" />
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
     );
 };
 
--- src/Pages/Shared/Navber.js
+++ src/Pages/Shared/Navber.js
@@ -1,15 +1,17 @@
 import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const Navbar = () => {
 
     const [user] = useAuthState(auth);
+    const navigate = useNavigate()
 
     const logout = () => {
         signOut(auth)
+        navigate('/')
         localStorage.removeItem('accessToken')
     }
 
