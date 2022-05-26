155ccda7cd2b159a3645894c4eeaa5bbe2f3167e
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-26 23:02:48 +0600

    my orders page has some order

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+
+    const [orders, setOrders] = useState([])
+    const [user] = useAuthState(auth)
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/order?email=${user.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data))
+        }
+    }, [user])
+
     return (
         <div>
-            <h1>My Orders</h1>
+            <h1>My Orders: {orders.length}</h1>
+
         </div>
     );
 };
